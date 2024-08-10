cfff07ef5ef364985a681458014912d9184413f4
Shilo <leoocode@gmail.com>
2024-08-10 12:58:13 -0600

    add clean up step

--- src/compute/ec2/amazon-machine-images/create-an-AMI/1-create-instance.sh
+++ src/compute/ec2/amazon-machine-images/create-an-AMI/1-create-instance.sh
@@ -1,5 +1,4 @@
 #Bash Script to create an EC2 instance using a standard AMI and customize it using user data.
-echo "Check if Stack already exists"
 TEMPLATE=$(aws cloudformation create-stack --stack-name my-ec2-instance-stack --template-body file://./create-cutomize-instance.yaml)
 
 echo "Stack created successfully"
--- src/compute/ec2/amazon-machine-images/create-an-AMI/6-clean-up.sh
+++ src/compute/ec2/amazon-machine-images/create-an-AMI/6-clean-up.sh
@@ -6,8 +6,17 @@ STACK1=$(aws cloudformation delete-stack --stack-name my-ec2-instance-stack)
 echo "Deleting the CloudFormation second stack"
 STACK2=$(aws cloudformation delete-stack --stack-name my-ec2-instance-from-custom-AMI-stack)
 
+echo "Getting the Snapshot ID to delete"
+
+IMAGEID=$(cat image-id.txt)
+SNAPSHOTID=$(aws ec2 describe-images --image-ids $IMAGEID --query 'Images[0].BlockDeviceMappings[0].Ebs.SnapshotId' --output text)
+
 echo "Deregister Custom Image"
-#TOFIX: This command is not working
+DEREGISTER_IMAGE=$(aws ec2 deregister-image --image-id $IMAGEID)
+
+
+echo "Delete Snapshot Image"
+DELETE_SNAPSHOT=$(aws ec2 delete-snapshot --snapshot-id $SNAPSHOTID)
 
 echo "Deleting the temporary files"
 
@@ -16,3 +25,5 @@ rm custom-key-pair.pem
 rm new-key-pair.pem
 
 echo "Clean up completed successfully"
+
+# If known host was added to the known_hosts file, remove it
